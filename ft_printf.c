8f1da647e20fcc9a774c2a4011dbbbc02cad65f8
Alvaro Martin Falagan <alvamart@c2r12s2.42madrid.com>
2024-10-18 12:06:20 +0200

    MOdificaciones

--- ft_printf.c
+++ ft_printf.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvamart <alvamart@student.42madrid.com>   #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-10-18 10:01:35 by alvamart          #+#    #+#             */
+/*   Updated: 2024-10-18 10:01:35 by alvamart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 int	check_params(char param, va_list arg)
@@ -16,7 +28,7 @@ int	check_params(char param, va_list arg)
 	else if (param == 'd' || param == 'i')
 		return ft_printf_itoa(va_arg(arg, int));
 	else if (param == 'u')
-		return ft_print_ubase(va_arg(arg, int), "0123456789");
+		return ft_print_ubase(va_arg(arg, unsigned int), "0123456789");
 	else if (param == 'x')
 		return ft_print_base(va_arg(arg, int), "0123456789abcdef");
 	else if (param == 'X')
